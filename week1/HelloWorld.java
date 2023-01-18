dc7be3e75a4e1935872656a3301eff5800d6544e
Adam Poliak <azpoliak@gmail.com>
2023-01-17 19:39:37 -0500

    helloworld-class1

---
+++ week1/HelloWorld.java
@@ -0,0 +1,12 @@
+// Name: Adam Poliak
+// Date: 01/17/23
+// Our very first java program - we are legit programmers!
+
+public class HelloWorld { // the name of the class must match (case-sensitive)
+			  // the name of the file (except for the .java)
+
+   public static void main(String[] args) {
+      System.out.println("Hello world!");
+      // System.out.println("Hello world!, I'm a comment"); // this line is commented out
+   }
+}
