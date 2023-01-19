448d37f8f02ead7e8f1def0ed3d439cf11f664ff
Adam Poliak <azpoliak@gmail.com>
2023-01-19 15:40:06 -0500

    lecture 02 code

---
+++ week1/Variables.java
@@ -0,0 +1,53 @@
+// Adam Poliak
+// 01/19/23
+// Playing with variables
+
+public class Variables {
+
+  public static void main(String[] args) {
+
+    System.out.println("Today we are playing with variables!");
+    System.out.println("Who is excited?");
+
+    // printing out variables
+    int firstVariable = 5;
+    System.out.println(firstVariable);
+    // GO BACK TO SLIDES (Variable Examples)
+
+   int a, b; // Declaration
+   // System.out.println(a);
+  String name = "coco";
+  System.out.println(name); 
+  a = 3; 
+  b = a;
+  System.out.println(a); 
+  System.out.println(b); 
+
+  a = 5;
+  System.out.println(a); 
+  System.out.println(b); 
+
+  //int int = 10;
+
+  // Operations
+  // division
+  System.out.println(10.0 / 4); // 2.5
+  System.out.println(10 / 4);  // 2
+
+  // modulo
+  System.out.println(10 % 9); // reaiminder is 1
+
+  int c = 10;
+  int d = 11;
+  System.out.println(c * d); // 10 *11
+
+
+  int result = 24 + 9 / 4 + (9 /2) + 10;
+  System.out.println(result);
+
+  // this casts the integer into a double
+  double result2 = 24 + 9 / 4 + (9 /2) + 10;
+  System.out.println(result2);
+
+  }
+}
